decd9f9fd7a8083bd82f9cf791ece4e27ddbf548
Mikhael Bogdanov <mikhael.bogdanov@jetbrains.com>
2017-03-14 13:52:07 +0100

    Move object initialization from <init> to <clinit>
      Codegen generates static backing fields for object properties.
      They are initialized in class constructor but some of them are final static
      and such access is prohibited in specification but it's allowed in
      java bytecode <= 1.8. Such access in 1.9 bytecode cause
      "IllegalAccessError: Update to static final field Object.INSTANCE
      attempted from a different method (<init>) than the initializer method <clinit>"
    
      Added additional hidden field in interface companion to pass out
      companion instance from <clinit>.
    
     #KT-15894 Fixed

--- compiler/tests-ir-jvm/tests/org/jetbrains/kotlin/codegen/ir/IrBlackBoxCodegenTestGenerated.java
+++ compiler/tests-ir-jvm/tests/org/jetbrains/kotlin/codegen/ir/IrBlackBoxCodegenTestGenerated.java
@@ -11963,6 +11963,12 @@ public class IrBlackBoxCodegenTestGenerated extends AbstractIrBlackBoxCodegenTes
             doTest(fileName);
         }
 
+        @TestMetadata("classCompanion.kt")
+        public void testClassCompanion() throws Exception {
+            String fileName = KotlinTestUtils.navigationMetadata("compiler/testData/codegen/box/objects/classCompanion.kt");
+            doTest(fileName);
+        }
+
         @TestMetadata("flist.kt")
         public void testFlist() throws Exception {
             String fileName = KotlinTestUtils.navigationMetadata("compiler/testData/codegen/box/objects/flist.kt");
@@ -11978,6 +11984,12 @@ public class IrBlackBoxCodegenTestGenerated extends AbstractIrBlackBoxCodegenTes
         @TestMetadata("interfaceCompanionObjectReference.kt")
         public void testInterfaceCompanionObjectReference() throws Exception {
             String fileName = KotlinTestUtils.navigationMetadata("compiler/testData/codegen/box/objects/interfaceCompanionObjectReference.kt");
+            doTest(fileName);
+        }
+
+        @TestMetadata("interfaceCompanion.kt")
+        public void testInterfaceCompanion() throws Exception {
+            String fileName = KotlinTestUtils.navigationMetadata("compiler/testData/codegen/box/objects/interfaceCompanion.kt");
             doTest(fileName);
         }
 
@@ -12116,6 +12128,12 @@ public class IrBlackBoxCodegenTestGenerated extends AbstractIrBlackBoxCodegenTes
         @TestMetadata("nestedObjectWithSuperclass.kt")
         public void testNestedObjectWithSuperclass() throws Exception {
             String fileName = KotlinTestUtils.navigationMetadata("compiler/testData/codegen/box/objects/nestedObjectWithSuperclass.kt");
+            doTest(fileName);
+        }
+
+        @TestMetadata("object.kt")
+        public void testObject() throws Exception {
+            String fileName = KotlinTestUtils.navigationMetadata("compiler/testData/codegen/box/objects/object.kt");
             doTest(fileName);
         }
 
